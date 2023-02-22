f0716e05b8a86604c0dc2b9d3c552a39bb12b71e
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-22 14:42:20 -0500

    add navbar

---
+++ communityBoard/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { SiFoodpanda } from 'react-icons/si'
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1><span className='icon'><SiFoodpanda /></span>Asian Eatery</h1>
+            <p>Best Halal Asian Food in NYC</p>
+        </nav>
+    )
+
+}
+
+export default Navbar;
